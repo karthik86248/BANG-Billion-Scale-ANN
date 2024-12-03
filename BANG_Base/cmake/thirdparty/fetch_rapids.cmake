de9f5009ac1ab7783d0e98c7a931064cc49ba2d1
Tarang Jain <jaintarang2015@gmail.com>
2024-12-03 00:49:20 -0800

    cmake changes

---
+++ BANG_Base/cmake/thirdparty/fetch_rapids.cmake
@@ -0,0 +1,7 @@
+set(RAPIDS_VERSION "24.10")
+
+if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/BANG_RAPIDS.cmake)
+    file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-${RAPIDS_VERSION}/RAPIDS.cmake
+            ${CMAKE_CURRENT_BINARY_DIR}/BANG_RAPIDS.cmake)
+endif()
+include(${CMAKE_CURRENT_BINARY_DIR}/BANG_RAPIDS.cmake)
